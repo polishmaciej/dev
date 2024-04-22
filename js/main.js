96930a9ea24dc16159605b685ec8b0612a553ce0
Maciek <Maciekzaqwsx@interia.pl>
2024-04-22 19:52:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
+
+const handleNav = () => {
+	nav.classList.toggle("nav--active");
+
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
+navBtn.addEventListener("click", handleNav);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+
+	function addShadow() {
+		if (window.scrollY >= 100) {
+			nav.classList.add("shadow-bg");
+		} else {
+			nav.classList.remove("shadow-bg");
+		}
+	}
+
+	window.addEventListener("scroll", addShadow);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const containers = document.querySelectorAll('.r-container, .r-container-2');
+
+    containers.forEach(container => {
+        let animationClass;
+
+        if (container.classList.contains('r-container')) {
+            animationClass = 'animation_right';
+        } else if (container.classList.contains('r-container-2')) {
+            animationClass = 'animation_left';
+        }
+
+        const slides = container.querySelectorAll('.reviews__slide');
+
+        container.addEventListener('mouseenter', () => {
+            slides.forEach(slide => {
+                slide.classList.add('animation_stop');
+            });
+        });
+
+        container.addEventListener('mouseleave', () => {
+            slides.forEach(slide => {
+                slide.classList.remove('animation_stop');
+            });
+        });
+    });
+});
+
